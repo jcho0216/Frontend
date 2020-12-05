8bb8fc715f88b5eaad75962fa43d2a7c8fddaebc
Cho Junseo <chojuneseo1@gmail.com>
2020-12-05 23:52:55 +0900

    To Do List only ADD

--- ToDoList/ToDoList.js
+++ ToDoList/ToDoList.js
@@ -1,10 +1,37 @@
-let list = document.getElementById('list');
+let list = document.querySelector('#list');
 let InputBox = document.querySelector('.input-box');
-let AddBtn = document.getElementsByClassName('add-btn');
+let delBtn = document.querySelector('.delBtn');
+let editBtn = document.querySelector('.editBtn');
 
-const buttonClick = () => {
-    list.innerHTML += ('<li>' + InputBox.value + '</li>');
+
+const AddList = () => {
+    let itemList = document.createElement('li');
+    let delBtn = document.createElement('img');
+    let editBtn = document.createElement('img');
+
+    delBtn.setAttribute('class', 'delBtn');
+    delBtn.setAttribute('src', "https://img.icons8.com/ios/50/000000/delete--v3.png");
+        
+    editBtn.setAttribute('class', 'editBtn');
+    editBtn.setAttribute('src', "https://img.icons8.com/ios/50/000000/create-new.png");
+
+    if(InputBox.value !== ""){
+        itemList.innerHTML = InputBox.value;
+        list.appendChild(itemList); 
+        itemList.appendChild(delBtn);
+        itemList.appendChild(editBtn);
+        InputBox.value = "";
+    }
+    else {
+        alert('할일이 없군요...');
+    }
+    
 }
+
+
+
+
+
 
 
 
