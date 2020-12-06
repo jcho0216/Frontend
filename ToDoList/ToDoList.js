8373313c30f4aadb7d134915aba4919e62271d0a
Cho Junseo <chojuneseo1@gmail.com>
2020-12-06 16:51:06 +0900

    구현 완료된 ToDoList
    추가, 삭제, 수정 구현완료

--- ToDoList/ToDoList.js
+++ ToDoList/ToDoList.js
@@ -1,8 +1,5 @@
 let list = document.querySelector('#list');
 let InputBox = document.querySelector('.input-box');
-let delBtn = document.querySelector('.delBtn');
-let editBtn = document.querySelector('.editBtn');
-
 
 const AddList = () => {
     let itemList = document.createElement('li');
@@ -16,22 +13,33 @@ const AddList = () => {
     editBtn.setAttribute('src', "https://img.icons8.com/ios/50/000000/create-new.png");
 
     if(InputBox.value !== ""){
-        itemList.innerHTML = InputBox.value;
+        itemList.innerHTML = InputBox.value; //인풋값 li안에 넣기
         list.appendChild(itemList); 
         itemList.appendChild(delBtn);
         itemList.appendChild(editBtn);
         InputBox.value = "";
-    }
-    else {
-        alert('할일이 없군요...');
+
+        delBtn.addEventListener('click', function() { //삭제버튼 클릭 이벤트
+            delBtn.parentElement.style.display="none";  
+        });
+
+        editBtn.addEventListener('click', function() { //수정버튼 클릭 이벤트
+            let editValue = prompt("수정된 값을 입력해주세요", this.parentNode.firstChild.nodeValue);
+            if(editValue !== "") {
+                this.parentNode.firstChild.nodeValue = editValue;
+            }
+        });
     }
     
+    else {
+        alert('할 일이 없군요...');
+    }
 }
 
 
-
-
-
-
-
+const enterKey = () => {
+    if(window.event.keyCode == 13){
+        AddList();
+    }
+}
 
