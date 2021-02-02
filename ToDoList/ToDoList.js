4c2ab27437f7ee86913c0b9f2ce7d3eecc1b2918
Cho Junseo <chojuneseo1@gmail.com>
2021-02-02 21:46:42 +0900

    To Do List

--- ToDoList/ToDoList.js
+++ ToDoList/ToDoList.js
@@ -42,17 +42,43 @@ forGetTime();
 
 const AddList = () => {
     const itemList = document.createElement('li'),
+        DateValue = document.createElement('h5'),
         delBtn = document.createElement('img'),
         editBtn = document.createElement('img');
 
+    let time = new Date;
+    let hour = time.getHours(),
+        minute = time.getMinutes(),
+        second = time.getSeconds();
+        
+        if(minute < 10){
+            minute = "0" + minute;
+        }
+        if(hour < 10){
+            hour = "0" + hour;
+        }
+        if(second < 10){
+            second = "0" + second;
+        }
+        
+        DateValue.innerText= hour + ":" + minute + ":" + second;
+    
     delBtn.setAttribute('class', 'delBtn');
     delBtn.setAttribute('src', "https://img.icons8.com/ios/50/000000/delete--v3.png");
 
     editBtn.setAttribute('class', 'editBtn');
     editBtn.setAttribute('src', "https://img.icons8.com/ios/50/000000/create-new.png");
 
+    DateValue.setAttribute('class', 'DateValue');
 
     delBtn.addEventListener('click', function() {
+        if(confirm("정말 삭제하시겠습니까? ") == true){
+            alert("삭제되었습니다~!");
+        }
+        else{
+            return;
+        }
+
         delBtn.parentElement.style.display="none";  
     });
 
@@ -72,6 +98,7 @@ const AddList = () => {
         list.appendChild(itemList); 
         itemList.appendChild(delBtn);
         itemList.appendChild(editBtn);
+        itemList.appendChild(DateValue);
         InputBox.value = "";
     }
     else {
